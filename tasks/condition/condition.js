3d1eea13adf9fd82f47e198b88f42d5e98a8212c
Siarhei Patapenka <Siarhei_Patapenka@epam.com>
2020-03-25 04:41:11 +0300

    Second Task

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -7,11 +7,12 @@
   const a = 3,
         b = 2;
 
-  if (a + b < 4) {
-    console.log('wrong');
-  } else {
-    console.log('too much');
-  }
+  console.log(a+b<4 ? "wrong" : "too much")
+  // SHOULD BE DELETED if (a + b < 4) {
+  // SHOULD BE DELETED  console.log('wrong');
+  // SHOULD BE DELETED} else {
+  // SHOULD BE DELETED  console.log('too much');
+  // SHOULD BE DELETED}
 }
 
 /** TODO
@@ -20,19 +21,41 @@
  * Task 1 - if else
  * Task 2 - ternary operator
  */
+
+ //  -=ternary operator=-
 {
     const x = 3;
     const y = 7;
+    (x<10 && y<10 ? z=x+y : z=x*y);
+    console.log('z1=',z);
 }
+
+//  -= if else=-
+{
+  const x = 3;
+  const y = 7;
+  if (x<10 && y<10) {
+      console.log('z2=', z=x+y);
+      } else {
+      console.log('z2=', z=x*y);
+      }
+  }
 
 /** TODO
  * The system receives 3 params - x, y, operator.
  * Operator can pass 4 possible values - "add", "subtract", "multiply", "divide".
  * Implement the logic when the system does appropriate operation based on passed operator param.
  */
-
 {
     const x = 3;
     const y = 7;
     const operator = "add";
+    if (operator =="add") {console.log('x+y=', x+y);
+    }
+    if (operator =="substract") {console.log('x-y=', x-y);
+    }
+    if (operator =="multiply") {console.log('x*y=', x*y);
+    }
+    if (operator =="divide") {console.log('x/y=', x/y);
+    }
 }
--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -20,28 +20,67 @@ function calculateDiscount(redemption) {
 
 /** TODO
  * implement factorial algorithm using for, while, do..while operators
- */
-{
+ */ 
+//using for
+ {
   const i = 10; //10! = 3628800
+  let x=1
+  for (let i = 1; i<=10; i++) {
+     (x=x*i);
+  }
+  console.log('factorial for =', x);   
+  
+}
+//using while
+{
+  let i = 10; //10! = 3628800
+  let x=1
+  while (i>=1) {
+      (x=x*i); 
+      i--;
+  }
+  console.log('factorial while=', x);   
+ }
+//using  do while
+ {
+  let i = 10; //10! = 3628800
+  let x=1
+  do { 
+      (x=x*i);
+      i--;
+   }
+   while (i >=1 );
+   console.log('factorial do while=', x); 
+   
 }
 
 /**
  * return concatenated string from an array of substring
  */
 {
+  let sum='';
   const substr = ["I", " love", " JS"];
+  for (let element of substr) {
+    sum=substr.join('');
+}
+console.log (sum);
 }
 
 /**
  * calculate a total of income of certain person
  */
-{
+{ let total=0;
   const personIncomes = {
     salary: 1985,
     rent: -600,
     interestOnDeposit: 250,
     otherExpences: -300
   };
+  for  (let property in personIncomes ) {
+    (total+=personIncomes[property]);
 }
+console.log ('Income is', total);
+}
+
 
 module.exports = calculateDiscount;
