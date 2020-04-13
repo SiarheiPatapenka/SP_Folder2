3270db4f0f51a46f60a958e5eeb935976989ca25
Siarhei Patapenka <Siarhei_Patapenka@epam.com>
2020-04-13 19:51:21 +0300

    Task 5 done

--- tasks/classes/classes.js
+++ tasks/classes/classes.js
@@ -4,7 +4,17 @@
  * 2) constructor that pass firstName and lastName to the object
  * 3) method getFullName
  */
-//put your code here
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName =lastName;
+    }
+    getFullName(){
+       return this.firstName + ' ' +  this.lastName;
+    }
+}
+
+
 
 /**
  * implement class Student that extends Person with:
@@ -12,10 +22,20 @@
  * 2) constructor that pass grade to the object
  * 3) method getGrade that returns property grade
  */
-//put your code here
+class Student extends Person{
+    constructor(firstName, lastName, grade) {  
+    super(firstName, lastName);
+    this.grade = grade;
+}
+getGrade() {
+    return this.grade;
+}  
+} 
 
-const person = //create instance of class person
-const student = //create instance of class student
+ //put your code here
+
+const person = new Person('Siarhei', 'Patapenka'); 
+const student = new Student('Siarhei', 'Patapenka', 'number');  //create instance of class student
 
 module.exports = {
     person,
