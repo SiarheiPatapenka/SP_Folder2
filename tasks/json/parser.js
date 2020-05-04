491214f051c19bca640b6a7240a5291b863ed2d9
Siarhei Patapenka <snatttch125@gmail.com>
2020-05-04 15:24:59 +0300

    Task7 done

--- tasks/json/parser.js
+++ tasks/json/parser.js
@@ -1,5 +1,5 @@
 /**
- * You need to write a parser, which takes the ./test.json file
+ * You need to write a parser, which takes the ./test.json file +
  * and will create a new JSON file with the name parsed.json with the structure:
  * [
  *  {
@@ -14,3 +14,28 @@
  * Please NOTE, that you should omit the .html extension
  * Please do the task in the scope of the file. Create a pull request a share it with your mentor.
  */
+const fs = require('fs');
+const objs= require('./test.json');
+
+ 
+const arr = Object.values(objs.list.entries);
+ console.log(arr);
+ 
+
+ function getNames(arr) {
+	const names = [];
+  for (ar of arr) {
+		names.push( {docID: 'http://doc.epam.com/' + ar.entry.name.replace('.html','')});
+  
+  }
+  return names;
+ }
+ 
+
+
+fs.writeFile('./parsed.json', JSON.stringify(getNames(arr), null, 4), err => {
+    if (err) {
+       throw err;
+     }
+   console.log('The file is created!');
+}); 
